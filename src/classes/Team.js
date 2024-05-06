5e3f7ef2f446ea36c50a15d9ffe516467d7332a8
neondoll <bonni_1998@mail.ru>
2024-05-06 16:25:00 +0300

    first commit

---
+++ src/classes/Team.js
@@ -0,0 +1,44 @@
+export default class Team {
+  constructor() {
+    this.members = new Set();
+  }
+
+  // Делаем класс итерируемым с помощью генератора
+  * [Symbol.iterator]() {
+    // Более простая реализация генератора
+    // yield* this.members.values();
+
+    // Собственная реализация генератора
+    const members = this.toArray();
+
+    let currentMemberIndex = 0;
+
+    while (currentMemberIndex < members.length) {
+      yield members[currentMemberIndex];
+
+      currentMemberIndex += 1;
+    }
+  }
+
+  add(character) {
+    if (this.members.has(character)) {
+      throw new Error('Персонаж уже добавлен в команду!');
+    }
+
+    return this.members.add(character);
+  }
+
+  addAll(...characters) {
+    characters.forEach((character) => {
+      if (!this.members.has(character)) {
+        this.members.add(character);
+      }
+    });
+
+    return this.members;
+  }
+
+  toArray() {
+    return [...this.members];
+  }
+}
